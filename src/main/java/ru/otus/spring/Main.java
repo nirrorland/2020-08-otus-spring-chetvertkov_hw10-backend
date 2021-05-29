6a3ad418a71559722cda8b16053769c3a660a69a
Kirill CHetvertkov <irmo.silindo@gmail.com>
2021-05-29 12:17:50 +0300

    - hw-18: defended by hystrix;

--- src/main/java/ru/otus/spring/Main.java
+++ src/main/java/ru/otus/spring/Main.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableAutoConfiguration
 public class Main {
     public static void main(String[] args) {
--- src/main/java/ru/otus/spring/service/BookServiceImpl.java
+++ src/main/java/ru/otus/spring/service/BookServiceImpl.java
@@ -1,11 +1,14 @@
 package ru.otus.spring.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.otus.spring.dao.BookDao;
 import ru.otus.spring.domain.Book;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,16 +21,23 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    @HystrixCommand(groupKey = "GenreService", fallbackMethod = "getCachedBookById", commandKey = "getBookById",
+            commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1")}
+    )
     public Book getById(long id) {
         return bookRepository.findById(id).orElse(null);
     }
 
     @Override
+    @HystrixCommand(groupKey = "GenreService", fallbackMethod = "getCachedBookByName", commandKey = "getBookByName",
+            commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1")})
     public Book getByName(String name) {
         return bookRepository.findByNameIgnoreCase(name).orElse(null);
     }
 
     @Override
+    @HystrixCommand(groupKey = "BookService", fallbackMethod = "getAllCachedBooks", commandKey = "getAllBooks",
+            commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1")})
     public List<Book> getAll() {
         return bookRepository.findAll();
     }
@@ -51,5 +61,17 @@ public class BookServiceImpl implements BookService {
         bookRepository.flush();
     }
 
+    public Book getCachedBookById(long id) {
+
+        return new Book(id,"Cached book" + id, null, null);
+    }
+
+    public Book getCachedBookByName(String name) {
+        return new Book(1,"Cached book" + name, null, null);
+    }
+
+    public List<Book> getAllCachedBooks() {
+        return new ArrayList<Book>();
+    }
 
 }
